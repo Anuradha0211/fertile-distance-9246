275fd012bd418bd1a47bf9b07f9cba85244715f2
Jatinkumar007 <jatin.kr9868@gmail.com>
2023-03-29 12:18:30 +0530

    day1-task

---
+++ payment/payment.js
@@ -0,0 +1,95 @@
+let LS=JSON.parse(localStorage.getItem("details"));
+if(LS==null){
+    LS=[]
+}
+check(LS)
+function check(LS){
+   let emaild=document.getElementById("email");
+   let named=document.getElementById("name");
+   let numberd=document.getElementById("number");
+   let pincoded=document.getElementById("pincode");
+   let locationd=document.getElementById("location");
+   let messaged=document.getElementById("message");
+   let cityd=document.getElementById("city");
+   let stated=document.getElementById("state");
+   let landmarkd=document.getElementById("landmark");
+   let altnumd=document.getElementById("sec-no");
+   let addressd=document.getElementById("address");
+   let btn=document.getElementById("addr-button");
+   let btn2=document.getElementById("login-button");
+
+   btn2.addEventListener("click",function(){
+    if(emaild.value==""){
+        alert("Enter Your Email")
+    }
+    else{
+        alert("Login Successful")
+    }
+   })
+
+
+ btn.addEventListener("click",function(){
+    let obj={
+        email:emaild.value,
+        name:named.value,
+        number:numberd.value,
+        pincode:pincoded.value,
+        location:locationd.value,
+        message:messaged.value,
+        city:cityd.value,
+        state:stated.value,
+        landmark:landmarkd.value,
+        alternateno:altnumd.value,
+        address:addressd.value
+       }
+
+       if(emaild.value==="" || named.value==="" || numberd.value==="" || pincoded.value==="" || locationd.value==="" || cityd.value===""){
+        alert("Enter Your Details")
+        }
+        else if(numberd.value.length!=10){
+        alert("Enter valid number")
+        }
+        else if(numberd.value.length==10){
+          let t=duplicate(numberd.value);
+          if(t){
+            alert("User is already exist you can directly go on login page")
+          }
+          else{
+        alert("Signup Successful")
+        LS.push(obj);
+        localStorage.setItem("details",JSON.stringify(LS));
+        //window.location.href = "./login.html"
+        }  
+      }        
+ })
+ function duplicate(a){
+    for(let k=0;k<LS.length;k++){
+      console.log(LS[k].number)
+      if(LS[k].number==a){
+        return true;
+      }
+    }
+    return false;
+  }
+   
+}
+function openform(){
+    document.querySelector(".login").style.display="block";
+    document.querySelector("#login-button").style.display="inline";
+}
+
+function closeform(){
+    document.querySelector(".login").style.display="none";
+    document.querySelector("#login-button").style.display="none";
+    //document.querySelector(".login > *").style.display="none";
+}
+
+function openform2(){
+    document.querySelector("#addr").style.display="block";
+    document.querySelector("#addr-button").style.display="inline";
+}
+
+function closeform2(){
+    document.querySelector("#addr-button").style.display="none";
+    document.querySelector("#addr").style.display="none";
+}
