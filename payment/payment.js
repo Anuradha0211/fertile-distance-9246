6179ca4bde7793c0fc45e92a7823f812fa3e7394
Jatinkumar007 <jatin.kr9868@gmail.com>
2023-03-31 22:52:06 +0530

    day4

--- payment/payment.js
+++ payment/payment.js
@@ -17,7 +17,55 @@ function check(LS){
    let btn=document.getElementById("addr-button");
    let btn2=document.getElementById("login-button");
    let div=document.getElementById("random");
+   let num=document.getElementById("no");
+   let num2=document.getElementById("randomcheck");
+   let btn3=document.getElementById("confirm");
+   let upi=document.getElementById("upi");
+   let hide=document.getElementById("hide");
+   let cash=document.getElementById("cash");
+   let align=document.getElementById("align");
+   let btn4=document.getElementById("product-button");
+   let paytm2=document.getElementById("paytm2");
+   let paytm3=document.getElementById("paytm3");
+   let hide2=document.getElementById("hide2");
+   let align2=document.getElementById("align2");
 
+
+   var x = Math.floor((Math.random()* 1000) + 1);
+   num.innerText=x;
+   div.append(num);
+   
+   upi.addEventListener("change",()=>{
+    hide.style.display="block";
+    align.style.display="none";
+   })
+
+   cash.addEventListener("change",()=>{
+    align.style.display="inline";
+    hide.style.display="none";
+   })
+
+   paytm2.addEventListener("change",()=>{
+    hide2.style.display="flex";
+    align2.style.display="none";
+   })
+
+   paytm3.addEventListener("change",()=>{
+    align2.style.display="block";
+    hide2.style.display="none";
+   })
+
+   btn3.addEventListener("click",()=>{
+    if(num2.value==x){
+      alert("ORDER CONFIRM SUCCESSFULLY")
+      document.querySelector("#payment").style.display="none";
+    }
+    else{
+      alert("Enter Valid OTP")
+      window.location.reload(num);
+    }
+   })
+    
 
    btn2.addEventListener("click",function(){
     if(emaild.value==""){
@@ -31,6 +79,11 @@ function check(LS){
     }
    })
 
+
+   btn4.addEventListener("click",()=>{
+    document.querySelector("#product").style.display="none";
+  document.querySelector("#product-button").style.display="none";
+   })
 
  btn.addEventListener("click",function(){
     let obj={
@@ -78,9 +131,7 @@ function check(LS){
   }
    
 }
- var x = Math.floor((Math.random()* 100) + 1);
- div.append(x);
-
+ 
 function openform(){
     document.querySelector(".login").style.display="block";
     document.querySelector("#login-button").style.display="inline";
@@ -91,3 +142,11 @@ function openform2(){
     document.querySelector("#addr-button").style.display="inline";
 }
 
+function openform3(){
+  document.querySelector("#product").style.display="block";
+  document.querySelector("#product-button").style.display="inline";
+}
+function openform4(){
+  document.querySelector("#payment").style.display="inline-block";
+  //document.querySelector("#product-button").style.display="inline";
+}
